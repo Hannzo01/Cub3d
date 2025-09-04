3077d2daa8b0c9df1b67dc4c7b6b84f96f1cf2e2
Hannzo01 <kenzamzouri24@gmail.com>
2025-09-04 01:25:02 +0100

    parsing is not parsing

---
+++ include/cub3d.h
@@ -0,0 +1,52 @@
+#ifndef CUB3D_H
+#define CUB3D_H
+
+# include "../gnl/get_next_line.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+
+typedef struct s_list
+{
+	char			*line;
+	struct	s_list	*next;
+}	t_list;
+
+typedef struct s_map
+{
+	int		map_height;
+	int		max_len;
+	char	**map;
+
+
+}	t_map;
+
+
+
+//-------------PARSING---------------//
+int		check_file_name(char *name);
+void	read_map(char *map, t_list **lst);
+void	allocate_and_fill_map(t_map *game, t_list *lst);
+void	print_map(char **map); //remove later
+
+
+
+
+
+//--------------UTILS----------------//
+void	ft_putstr_fd(char *s, int fd);
+int		ft_strlen(char *s);
+int		ft_strcmp(char *s1, char *s2);
+char	*ft_strrchr(char *s, int c);
+char	*ft_strcpy(char *dst, char *src);
+
+
+void	lstadd_back(t_list **lst, char *line);
+t_list	*lst_new(char *line);
+void	print_list(t_list *lst);  //remove later
+void	map_size(t_list *lst, t_map *game);
+
+
+#endif
---
+++ parsing/utils.c
@@ -0,0 +1,70 @@
+#include "../include/cub3d.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	if (s == NULL)
+		return ;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write (fd, &s[i], 1);
+		i++;
+	}
+}
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int ft_strcmp(char *s1, char *s2)
+{
+    int i;
+
+    if (!s1 || !s2)
+        return (-1);
+
+    i = 0;
+    while (s1[i] && s1[i] == s2[i])
+        i++;
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+char	*ft_strrchr(char *s, int c)
+{
+	char	*p;
+
+	p = NULL;
+	while (*s)
+	{
+		if (*s == (char)c)
+			p = (char *)s;
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (p);
+}
+
+char	*ft_strcpy(char *dst, char *src)
+{
+    int	i;
+
+	i = 0;
+    if (!dst || !src)
+        return NULL;
+    while (src[i])
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (dst);
+}
