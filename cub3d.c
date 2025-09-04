2b96ba847354cab230c811eed61ed0542ac1a39a
saiechlahbi <echlahbisaid@gmail.com>
2025-09-04 23:09:04 +0100

    lhma9 hada

---
+++ cub3d.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 17:02:21 by sechlahb          #+#    #+#             */
+/*   Updated: 2025/09/04 23:06:59 by sechlahb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// -L/usr/include/minilibx-linux -lmlx -lX11 -lXext 
+
+// #include "mlx.h"
+#include "/home/sechlahb/Desktop/minilibx-linux/mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_data
+{
+    char **map;
+    char *north_texture;
+    char *south_texture;
+    char *west_texture;
+    char *east_texture;
+    int *F;
+    int *C;
+    // F 220,100,0 exemple for floor color  -- F mean floor
+    // C 225,30,0 exemple for Ceiling color -- C mean ceiling
+} t_data;
+
+typedef struct s_cub3d
+{
+    void *mlx;
+    void *window;
+    t_data *data;
+    
+} t_cub3d;
+
+int handler(t_cub3d *cub3d)
+{
+    (void) cub3d;
+    printf("gamme closed\n");
+    exit (0);
+}
+
+int main()
+{
+    t_cub3d cub3d;
+    
+    char map[10][11];
+    map[0] = "1111111111";
+    map[1] = "1000000001";
+    map[2] = "1001110001";
+    map[3] = "1000000001";
+    map[4] = "1000000001";
+    map[5] = "1000000001";
+    map[6] = "1000N00001";
+    map[7] = "1000000001";
+    map[8] = "1111111111";
+    map[9] = NULL;
+    
+    cub3d.mlx = mlx_init();
+    cub3d.window = mlx_new_window(cub3d.mlx, 800, 600, "cub3d");
+    mlx_hook(cub3d.window, 17, 0, handler, &cub3d);
+    mlx_loop(cub3d.mlx);
+}
